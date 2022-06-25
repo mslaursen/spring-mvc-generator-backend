fc5ebf6482d1a9481bf0bece0ebfd3be123f1a52
martinlaursen97 <martin_laursen21@hotmail.com>
2022-06-25 12:45:20 +0200

    can now print relations

--- src/main/java/com/code/springmvcgenerator/service/EntityDetailService.java
+++ src/main/java/com/code/springmvcgenerator/service/EntityDetailService.java
@@ -28,21 +28,62 @@ public class EntityDetailService {
                 .append(spaces)
                 .append("private Long id\n");
 
+
         // Relationship columns
 
+        for (Vector3S v : entityDetail.getRelations()) {
+            sb.append(spaces);
+            switch (v.getVal1()) {
+                case "ManyToOne", "OneToOne" -> sb
+                        .append("\n")
+                        .append(spaces)
+                        .append("@")
+                        .append(v.getVal1())
+                        .append("(cascade = CascadeType.MERGE)\n")
+                        .append(spaces)
+                        .append("@JoinColumn(name = \"")
+                        .append(v.getVal2().toLowerCase())
+                        .append("_id\")\n")
+                        .append(spaces)
+                        .append("private ")
+                        .append(v.getVal2())
+                        .append(" ")
+                        .append(v.getVal2().toLowerCase())
+                        .append(";\n");
+                case "OneToMany" -> sb
+                        .append("\n")
+                        .append(spaces)
+                        .append("@JsonBackReference(value = \"")
+                        .append(v.getVal3().toLowerCase())
+                        .append("\")\n")
+                        .append(spaces)
+                        .append("@")
+                        .append(v.getVal1())
+                        .append("(mappedBy = \"")
+                        .append(entityDetail.getEntityName().toLowerCase())
+                        .append("\", cascade = CascadeType.MERGE)\n")
+                        .append(spaces)
+                        .append("@ToString.Exclude\n")
+                        .append(spaces)
+                        .append("private List<")
+                        .append(v.getVal2())
+                        .append("> ")
+                        .append(v.getVal3().toLowerCase())
+                        .append(";\n");
+            }
+        }
 
 
         // Columns
         for (Vector3S v : entityDetail.getVariables()) {
-            sb.append("    ");
+            sb.append(spaces);
 
             if (v.getVal3() != null) {
                 sb.append("\n")
                         .append(spaces)
                         .append("@Column(name = \"")
                         .append(v.getVal3())
-                        .append("\")")
-                        .append("\n");
+                        .append("\")");
             }
 
             sb.append("\n")
